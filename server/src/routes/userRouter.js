73b8cc6aa2c9cffde782e4df31fc557c6c89308d
tm-ahad <tm.ahad.07@gmail.com>
2022-07-24 10:56:21 +0600

    User find successfully

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('../controller/userController');
 
 router
-    .post('/create', userController.createUser);
+    .post('/create', userController.createUser)
+    .get('/find', userController.findUser);
 module.exports = {router};
